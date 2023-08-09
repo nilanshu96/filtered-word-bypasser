3cee95fc93757f73d7fa6590a8efca4c2ead16db
Nilanshu Vithal Rajmane <rajmanenilanshu@gmail.com>
2023-08-09 15:10:22 +0530

    upgraded version number

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "v2";
+const CACHE_NAME = "v3";
 
 //installs the service worker which then saves the files used by the game into the cache
 self.addEventListener('install', event => {
